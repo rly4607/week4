573048f7be38f997d3896bb5f59553958ec143a4
Ryan Lane <ryan.lane@kellogg.northwestern.edu>
2017-01-29 14:19:43 -0600

    FadOut of Task List

--- todo/todo.js
+++ todo/todo.js
@@ -1,11 +1,13 @@
 $(function() {
 
   $(".task").on("click", function(event) {
+
     event.preventDefault()
+    console.log("this function ran")
     var elementThatWasClicked = $(this)
     console.log(elementThatWasClicked)
-  //  elementThatWasClicked.remove()
-    elementThatWasClicked.parent().remove()
+    //  elementThatWasClicked.remove()
+    elementThatWasClicked.parent().parent().fadeOut()
     // remove the element
   })
 
