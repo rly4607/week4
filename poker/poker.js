8c4c18f83edfe3e6a4ad82f0a56dbc12b2f9dacd
Ryan Lane <ryan.lane@kellogg.northwestern.edu>
2017-01-28 20:07:15 -0600

    Show Results

--- poker/poker.js
+++ poker/poker.js
@@ -44,8 +44,135 @@ $(function() {
       $("#c" + (i+1)).attr("src", "http://golearntocode.com/images/cards/" + newShuffle[i] +".png")
     }
 
+    window.cards = cards
 
-    $("#hand").html("<h1>Result: Flush!</h1>")
+    // Count up number of cards & suits
+
+    var counter = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    var suits = 0
+    var Result = "Nuttin!"
+
+    for (j=2; j<=14; j++) {
+      for (k=0; k<=4; k++) {
+        if (j<=10) {
+          if (cards[k][0] == j) {
+            counter[j]++
+            Result = j + " High... :-("
+            }
+        }
+        else {
+          switch (j) {
+            case 11:
+              if(cards[k][0] == "jack") {
+                counter[j]++
+                Result = "Jack High!"
+              }
+              break;
+            case 12:
+              if(cards[k][0] == "queen") {
+                counter[j]++
+                Result = "Queen High!"
+              }
+              break;
+            case 13:
+              if(cards[k][0] == "king") {
+                counter[j]++
+                Result = "King High!"
+              }
+              break;
+            case 14:
+              if(cards[k][0] == "ace") {
+                counter[j]++
+                counter[1]++
+                Result = "Ace High!"
+              }
+              break;
+          }
+        }
+      }
+    }
+
+    console.log(counter)
+
+    // check to see if cards 2,3,4,5 suit matches card 1
+    // suit of card 1 doesn't really matter since all suits
+    // have to match for a flush
+
+    for (m=1; m<=4; m++) {
+      if(cards[m][1]==cards[0][1]) {
+        suits++
+      }
+    }
+    console.log("suit counter is " + suits)
+
+    var twos = 0
+    var threes = 0
+    var fours = 0
+    var straight = 0
+
+    // Check for two of a kind, three of a kind, four of a kind, two pair,
+    // full house, and a straight
+
+    for (l=0; l<counter.length; l++) {
+      if(counter[l]==1 && l<(counter.length-1)) {
+        if(counter[l+1]==1) {
+          straight++
+          console.log("counter position is " + l + " straight counter = " + straight)
+        }
+      }
+      else if(straight>0) {
+        straight--
+      }
+
+      if(counter[l]==2 && l>1)
+      {
+        twos++
+      }
+      if(counter[l]==3 && l>1)
+      {
+        threes++
+      }
+      if(counter[l]==4 && l>1)
+      {
+        fours++
+      }
+    }
+
+//    console.log("twos = " + twos + " threes = " + threes + " fours = " + fours)
+
+
+    if(twos==1) {
+      Result = "Two of a Kind!"
+    }
+    if(twos==2) {
+      Result = "Two Pair!"
+    }
+    if(threes==1) {
+      Result = "Three of a Kind!"
+    }
+    if(twos==1 && threes==1) {
+      Result = "Full House!"
+    }
+    if(fours)
+    {
+      Result = "Four of a Kind!"
+    }
+    if(straight==4 || suits==4)
+    {
+      if(straight==4 && suits==4) {
+        Result = "Straight Flush!"
+      }
+      else if(straight==4){
+        Result = "Straight!"
+      }
+      else {
+        Result = "Flush!"
+      }
+    }
+
+    console.log(Result)
+
+    $("#hand").html("<h1>Result: " + Result + "</h1>")
 
   });
 });
