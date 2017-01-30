9ba1da25e740c66ecc96e06853221a26a83d164d
Ryan Lane <ryan.lane@kellogg.northwestern.edu>
2017-01-29 18:32:27 -0600

    Added Comments & Cleanup

--- todo/todo.js
+++ todo/todo.js
@@ -9,39 +9,34 @@ $(function() {
     event.preventDefault()
     var elementThatWasClicked = $(this)
     window.newvar = elementThatWasClicked
-//    console.log(elementThatWasClicked)
-//    console.log(newvar)
+
     var itemNum = elementThatWasClicked[0].id.split("task")[1]
 
     console.log("Item # " + itemNum + " was clicked")
 
     $("#status" + itemNum).html("Done!")
     $("#due" + itemNum).html("<a>Remove from list?</a>")
+    // Put a strikethrough the task text
     $(this).css("text-decoration","line-through")
 
-
-  //  elementThatWasClicked.parent().parent().fadeOut()
-    // remove the element
-
-  /*  var color = $( this ).css( "font-size" );
-    console.log( "That div is " + color );
-    window.color = color
-    var newcolor = (parseInt(color.split("px")[0])+2)
-    console.log("New font size is " + newcolor)
-    $(this).css("font-size",newcolor + "px") */
   })
 
   $(".Remove").on("click", function(event) {
-    console.log("Remove element was clicked")
-
     var elementThatWasClicked = $(this)
     window.newvar = elementThatWasClicked
-    if (newvar[0].innerText == "Remove from list?") {
-      elementThatWasClicked.parent().parent().fadeOut()
-      var itemNum = parseInt(elementThatWasClicked[0].id.split("due")[1])
 
-      //console.log("Item # " + itemNum + " was clicked")
-      var nextItem = itemNum + 1
+    // First check to see if the task has already been completed and the text
+    // has been changed.  It's changed if it equals "Remove from list?"
+    // If not - do nuthin'
+
+    if (newvar[0].innerText == "Remove from list?") {
+
+      // Remove row from the table
+      elementThatWasClicked.parent().parent().fadeOut()
+
+      // Go through items after this one and decrement their Task #s
+      var itemNum = parseInt(elementThatWasClicked[0].id.split("due")[1])
+      var nextItem = 0
       var nextItemVal = 0
 
       for(i=itemNum; i<7; i++) {
